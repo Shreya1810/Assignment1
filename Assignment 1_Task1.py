c8944865803857899f39f99144be00f85ac1e0d9
Shreya1810 <66972361+Shreya1810@users.noreply.github.com>
2025-06-14 17:09:39 +0530

    Add files via upload

---
+++ Assignment 1_Task1.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+
+add = x+y
+sub = x-y
+mul = x*y
+print("\nAddition : ", add)
+print("Subtraction : ",sub )
+print("Multiplication : ", mul)
+print("Division : ", x/y)
+
